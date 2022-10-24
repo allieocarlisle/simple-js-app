bd8da191fc39ab7550023507c74c2e64f0a63c91
allieocarlisle <112963162+allieocarlisle@users.noreply.github.com>
2022-10-24 09:40:40 -0400

    created Pokemon List array
    array contains objects and properties like name, height, and type

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-alert('Hello world!');
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 7,
+    type: [
+      "grass","monster"
+    ]
+  },
+  {
+    name: "Charmander",
+    height: 6,
+    type: [
+      "dragon","monster"
+    ]
+  },
+  {
+    name: "Squirtle",
+    height: 5,
+    type: [
+      "water", "monster"
+    ]
+  }
+];
