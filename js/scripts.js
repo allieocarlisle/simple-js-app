af4a3e58c08b033808065fa93256c873797d97ef
allieocarlisle <112963162+allieocarlisle@users.noreply.github.com>
2022-11-04 09:39:02 -0400

    updated the for loop to a forEach() function
    specific to the pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -22,11 +22,20 @@ let pokemonList = [
   }
 ];
 
-//start loop for pokemonList array:
-for (let i=0; i < pokemonList.length; i++){
-  document.write('<p>' + pokemonList[i].name + ': (height: ' + pokemonList[i].height + ') </p>')
-//below is height conditional:
-  if (pokemonList[i].height > 6){
-  document.write('Wow, ' + pokemonList[i].name + ' is a big pokemon!')
+// //start loop for pokemonList array:
+// for (let i=0; i < pokemonList.length; i++){
+//   document.write('<p>' + pokemonList[i].name + ': (height: ' + pokemonList[i].height + ') </p>')
+// //below is height conditional:
+//   if (pokemonList[i].height > 6){
+//   document.write('Wow, ' + pokemonList[i].name + ' is a big pokemon!')
+// }
+// }
+
+//the code below uses the forEach() function to iterate over the Pokemon in my pokemonList.
+function printArrayDetails(pokemon) {
+  document.write('<p>' + pokemon.name + ' is ' + pokemon.height + ' m tall.</p>');
+  if (pokemon.height > 6) {
+    document.write('Wow - that\'s a big pokemon!')
+  }
 }
-}
+pokemonList.forEach(printArrayDetails);
