78c222e404ffc75912dd55d151717e4522ca869f
allieocarlisle <112963162+allieocarlisle@users.noreply.github.com>
2022-12-01 09:39:18 -0500

    updated repository to load data via external source

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,7 @@
 //below is the repository pattern, involving IIFE, to return and add all data
 let pokemonRepository = (() => {
-  let pokemonList = [
-    {
-      name: "Bulbasaur",
-      height: 7,
-      type: [
-        "grass","monster"
-      ]
-    },
-    {
-      name: "Charmander",
-      height: 6,
-      type: [
-        "dragon","monster"
-      ]
-    },
-    {
-      name: "Squirtle",
-      height: 5,
-      type: [
-        "water", "monster"
-      ]
-    },
-    {
-      name: "Pikachu",
-      height: 4,
-      type: [
-        "field", "fairy"
-      ]
-    },
-    {
-      name: "Jigglypuff",
-      height: 5,
-      type: "fairy"
-    },
-    {
-      name: "Tauros",
-      height: 14,
-      type: "field"
-    }
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     pokemonList.push(pokemon);
@@ -63,21 +25,55 @@ let pokemonRepository = (() => {
   }
 
   function showDetails(pokemon){
-    console.log(pokemon);
-  };
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
+  }
+
+  function loadList(){
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item){
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
 
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
   };
 })();
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
-// pokemonRepository.getAll().forEach(printArrayDetails);
-//end of repository pattern
 
 
 
