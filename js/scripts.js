9778d6f6f57a6208a4df2fe2a80b832a07cddd83
allieocarlisle <112963162+allieocarlisle@users.noreply.github.com>
2023-01-06 11:50:00 -0500

    edit class to call button on scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ let pokemonRepository = (() => {
 
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-  let myPokemonList = $('pokemon-list');
+  let myPokemonList = $('.pokemon-list');
 
 
   function add(pokemon) {
@@ -51,8 +51,9 @@ let pokemonRepository = (() => {
       return response.json();
     }).then(function (details) {
       item.imageUrl = details.sprites.front_default;
+      item.imageBackUrl = details.sprites.back_default;
       item.height = details.height;
-      item.types = details.types;
+      item.types = details.types.map(types => types.type.name);
       item.abilities = details.abilities.map(abilities => abilities.ability.name);
     }).catch(function (e) {
       console.error(e);
@@ -73,15 +74,18 @@ let pokemonRepository = (() => {
     modalTitle.empty();
     modalBody.empty();
  
-    let nameElement = $("<h1>" + pokemon.name + "</h1>");
+    let nameElement = $("<h3>" + pokemon.name + "</h3>");
     let imageElementFront = $('<img class="modal-img" style="width:50%">');
     imageElementFront.attr("src", pokemon.imageUrl);
-    let heightElement = $("<p>" + "height : " + pokemon.height + "</p>");
-    let typesElement = $("<p>" + "types : " + pokemon.types + "</p>");
-    let abilitiesElement = $("<p>" + "abilities : " + pokemon.abilities + "</p>");
+    let imageElementBack = $('<img class="modal-img-back" style="width:50%">');
+    imageElementBack.attr("src", pokemon.imageBackUrl);
+    let heightElement = $("<p>" + "Height: " + pokemon.height + "m" + "</p>");
+    let typesElement = $("<p>" + "Types: " + pokemon.types + "</p>");
+    let abilitiesElement = $("<p>" + "Abilities: " + pokemon.abilities + "</p>");
  
-    modalTitle.append(nameElement);
-    modalBody.append(imageElementFront);
+    modalTitle.append(imageElementFront);
+    modalTitle.append(imageElementBack);
+    modalBody.append(nameElement);
     modalBody.append(heightElement);
     modalBody.append(typesElement);
     modalBody.append(abilitiesElement);
